977611563efeb701744bb443cf5993703700e706
MermetM <58287046+MermetM@users.noreply.github.com>
2019-11-28 09:00:03 +0100

    Suppression du depot log

--- 1/src/main/java/log/Constante.java
+++
@@ -1,7 +0,0 @@
-package log;
-
-public class Constante {
-	final static String SuccessPath = "H:\\Documents\\Success.txt";
-	final static String ErrorPath = "H:\\Documents\\Error.txt";
-	final static String ModifPath = "H:\\Documents\\Modif.txt";
-}
