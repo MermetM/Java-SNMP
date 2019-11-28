8a6e7a76c153b58b4ce979f6ed11a29567453f47
MermetM <58287046+MermetM@users.noreply.github.com>
2019-11-28 08:27:55 +0100

    Add files via upload

---
+++ 1/src/main/java/log/Constante.java
@@ -0,0 +1,7 @@
+package log;
+
+public class Constante {
+	final static String SuccessPath = "H:\\Documents\\Success.txt";
+	final static String ErrorPath = "H:\\Documents\\Error.txt";
+	final static String ModifPath = "H:\\Documents\\Modif.txt";
+}
